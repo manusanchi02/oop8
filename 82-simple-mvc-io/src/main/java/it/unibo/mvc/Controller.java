2194bf90b81560c05a4d9a6d40ba026650a3c6bc
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2022-11-15 16:03:00 +0100

    Quite finished

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,34 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
 
+    private File currFile;
+
+    public Controller(String path) {
+        currFile = new File(path);
+    }
+
+    public File getFile() {
+        return currFile;
+    }
+
+    public String getPath() {
+        return currFile.getAbsolutePath();
+    }
+
+    public void writeOnFile(String s) throws IOException{
+        try (PrintStream ps = new PrintStream(currFile.getPath(), StandardCharsets.UTF_8)){
+            ps.print(s);
+        } catch (IOException e) {
+            System.out.println("Errore: " + e.getMessage());
+        }
+    }
 }
--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,13 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,5 +16,38 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+    private final int PROPORTION = 5;
 
+    public SimpleGUI() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.setLocationByPlatform(true);
+        JTextArea jt = new JTextArea();
+        JButton saveBtn = new JButton("Save");
+        frame.setLayout(new BorderLayout());
+        frame.add(saveBtn, BorderLayout.SOUTH);
+        frame.add(jt, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        /*Handlers*/
+        Controller c = new Controller("output.txt");
+        saveBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                int n = JOptionPane.showConfirmDialog(frame, "Do you want to save the file?", "Saving", JOptionPane.YES_NO_OPTION);
+                if (n == JOptionPane.YES_OPTION) {
+                    try {
+                        c.writeOnFile(jt.getText());
+                    } catch (IOException e) {
+                        System.out.println("Errore: " + e.getMessage());
+                    }
+                }
+            }
+        });
+    }
+
+    private void Display() {
+        frame.setVisible(true);
+    }
 }
